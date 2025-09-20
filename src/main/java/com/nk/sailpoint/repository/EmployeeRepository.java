5f048553244583d8caac1d30add08b1b4a4807f4
rajputnikesh <yournewemail@example.com>
2025-09-20 17:35:07 +0530

    updated

--- src/main/java/com/nk/sailpoint/repository/EmployeeRepository.java
+++ src/main/java/com/nk/sailpoint/repository/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.nk.sailpoint.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nk.sailpoint.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, String> {
-
+	Page<Employee> findAll(Pageable pageable);
 }
